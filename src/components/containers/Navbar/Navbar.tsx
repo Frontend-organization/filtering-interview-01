be69514dfd16ca9fe9290ee8f4b6348f1c0f19b1
Manuel Garcia <manueldev250799@gmail.com>
2021-07-31 19:53:17 -0500

    test: add navbar tests

--- src/components/containers/Navbar/Navbar.tsx
+++ src/components/containers/Navbar/Navbar.tsx
@@ -1,20 +1,28 @@
 import IconMenu from '@assets/icons/menu.svg'
 import IconLogo from '@assets/icons/logo.svg'
 import Spacer from '@components/common/Spacer'
+import { ComponentProps } from 'react'
 
 import styles from './Navbar.style.module.css'
 
-type Props = {
+type Props = ComponentProps<'section'> & {
   logo?: boolean
   menuButton?: boolean
   navbarRight?: JSX.Element
 }
 
-const Navbar: React.FC<Props> = ({ logo, menuButton, navbarRight }) => {
+const Navbar: React.FC<Props> = ({
+  logo,
+  menuButton,
+  navbarRight,
+  ...rest
+}) => {
   return (
-    <section className={styles.navbar}>
+    <section className={styles.navbar} {...rest}>
       <div className={styles.navbar__left}>
-        {menuButton ? <IconMenu fill="#fff" width={32} height={32} /> : null}
+        {menuButton ? (
+          <IconMenu fill="#fff" width={32} height={32} data-testid="btn-menu" />
+        ) : null}
         <Spacer x={10} />
         {logo ? <IconLogo fill="#fff" width={43} height={30} /> : null}
       </div>
