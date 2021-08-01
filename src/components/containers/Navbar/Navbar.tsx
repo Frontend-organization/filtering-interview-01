c65c35d127d562a8165e5346121b9c5d3dacdca0
Christian Rafael Guado <crguado@gmail.com>
2021-07-31 19:16:15 -0500

    feat: add navbar component wip on tests

--- src/components/containers/Navbar/Navbar.tsx
+++ src/components/containers/Navbar/Navbar.tsx
@@ -1,26 +1,24 @@
 import IconMenu from '@assets/icons/menu.svg'
 import IconLogo from '@assets/icons/logo.svg'
-import IconSearch from '@assets/icons/search.svg'
-import IconShopping from '@assets/icons/shopping.svg'
-
 import Spacer from '@components/common/Spacer'
 
 import styles from './Navbar.style.module.css'
 
-const Navbar: React.FC = () => {
+type Props = {
+  logo?: boolean
+  menuButton?: boolean
+  navbarRight?: JSX.Element
+}
+
+const Navbar: React.FC<Props> = ({ logo, menuButton, navbarRight }) => {
   return (
     <section className={styles.navbar}>
       <div className={styles.navbar__left}>
-        <IconMenu fill="#fff" width={32} height={32} />
+        {menuButton ? <IconMenu fill="#fff" width={32} height={32} /> : null}
         <Spacer x={10} />
-        <IconLogo fill="#fff" width={43} height={30} />
+        {logo ? <IconLogo fill="#fff" width={43} height={30} /> : null}
       </div>
-
-      <div className={styles.navbar__right}>
-        <IconSearch fill="#fff" width={32} height={32} />
-        <Spacer x={10} />
-        <IconShopping fill="#fff" width={32} height={32} />
-      </div>
+      <div className={styles.navbar__right}>{navbarRight}</div>
     </section>
   )
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,8 @@
 import styles from './Home.module.css'
 
-import Navbar from '../../components/containers/Navbar'
-
 const Home: React.FC = () => {
   return (
     <section className={styles.container}>
-      <Navbar></Navbar>
       <h2>Hello from Home.tsx</h2>
       <p>Lets code!</p>
     </section>
