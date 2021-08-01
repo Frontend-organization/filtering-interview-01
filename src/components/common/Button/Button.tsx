cec315fefe95b76065eec9b9d036c31c2f5ed552
ManuelDev <33798495+fullsnack-coder@users.noreply.github.com>
2021-07-31 21:06:38 -0500

    refactor: add new button styles (#8)

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -3,6 +3,8 @@ import { ComponentProps, ReactElement } from 'react'
 import styles from './Button.style.module.css'
 
 type Props = ComponentProps<'button'> & {
+  rounded?: boolean
+  fullWidth?: boolean
   variant?: 'primary' | 'secondary'
   renderLeft?: ReactElement
   renderRight?: ReactElement
@@ -10,15 +12,19 @@ type Props = ComponentProps<'button'> & {
 
 const Button: React.FC<Props> = ({
   children,
+  fullWidth,
   renderLeft,
   renderRight,
+  rounded,
   type = 'button',
   variant = 'secondary',
   ...rest
 }) => {
-  const buttonStyles = `${styles.button} ${
+  const buttonStyles = `${styles.button} 
+  ${rounded && styles.rounded} ${fullWidth && styles.full} ${
     variant === 'primary' ? styles.primary : styles.secondary
   }`
+
   return (
     <button className={buttonStyles} type={type} {...rest}>
       {!!renderLeft && <div className={styles.leftElement}>{renderLeft}</div>}
