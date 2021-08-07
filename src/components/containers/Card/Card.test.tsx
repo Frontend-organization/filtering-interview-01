2f653cfc780bfa9613d2eebe1139df65c9279d64
Christian <crguado@gmail.com>
2021-08-06 21:57:40 -0500

    Feature/card (#11)
    * feat: add component card
    
    * refactor: fixing the Card component
    
    * test: add card test and fix price format
    
    * feat: getBeers service and custom hook useBeers added (#13)
    
    * feat: getBeers service and custom hook useBeers added
    
    * test: add service & hook test
    
    Co-authored-by: Manuel Garcia <manueldev250799@gmail.com>
    
    * Revert "feat: getBeers service and custom hook useBeers added (#13)"
    
    This reverts commit 6ae72db55a51c427245765c2ea9cfd0aa544d17a.
    
    Co-authored-by: Manuel Garcia <manueldev250799@gmail.com>
    Co-authored-by: ManuelDev <33798495+fullsnack-coder@users.noreply.github.com>

---
+++ src/components/containers/Card/Card.test.tsx
@@ -0,0 +1,33 @@
+import { act, fireEvent, render } from '@testing-library/react'
+
+import Card, { Product } from './Card'
+
+const testProduct: Product = {
+  description: 'some description very large for testing purposses',
+  image: 'https://www.whatever-uri.com',
+  price: 50000,
+  title: 'Test title',
+  filterId: 'morena'
+}
+
+describe('<Card />', () => {
+  it('should render without crashing', () => {
+    const { getByText, getByAltText } = render(<Card product={testProduct} />)
+    expect(getByText(testProduct.title)).toBeDefined()
+    expect(getByAltText(testProduct.title.toLowerCase())).toBeInTheDocument()
+    expect(getByText(testProduct.description)).toBeDefined()
+    expect(getByText('AGREGAR')).toBeDefined()
+  })
+
+  it('should call addFunction with product when tap "ADD" button', () => {
+    const addProduct = jest.fn()
+    const { getByText } = render(
+      <Card product={testProduct} addProduct={addProduct} />
+    )
+    const addButton = getByText('AGREGAR')
+    act(() => {
+      fireEvent.click(addButton)
+    })
+    expect(addProduct).toHaveBeenCalledWith(testProduct)
+  })
+})
