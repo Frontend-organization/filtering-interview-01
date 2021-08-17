e5ac45c738ea16282ff2f29a55bcd3cad21116f7
Christian Rafael Guado <crguado@gmail.com>
2021-08-16 21:38:10 -0500

    contact section finished

--- src/components/containers/Footer/Footer.tsx
+++ src/components/containers/Footer/Footer.tsx
@@ -1,10 +1,45 @@
 import NavigationOptions from '../NavigationOptions'
+import Typography from '@components/common/Typography'
+import IconChat from '@assets/icons/chat.svg'
+import IconPhone from '@assets/icons/phone.svg'
+import Button from '@components/common/Button'
+import Flex from '@components/common/Flex'
+import Spacer from '@components/common/Spacer'
 
 import styles from './Footer.style.module.css'
 
 const Footer = () => {
   return (
     <div className={styles.Footer}>
+      <div className={styles.Contact}>
+        <Typography.Heading weight="bold">
+          ¿Podemos ayudarle?
+        </Typography.Heading>
+        <Spacer y={18} />
+        <Flex justifyContent="space-around">
+          <div className={styles.ButtonContainer}>
+            <Button
+              fullWidth
+              rounded
+              className={styles.ButtonClick}
+              style={{ padding: 22, width: 'fit-content' }}
+            >
+              <IconChat width={30} height={28} fill="#000" />
+            </Button>
+            <Typography.P weight="bold">CHAT</Typography.P>
+          </div>
+          <div className={styles.ButtonContainer}>
+            <Button
+              rounded
+              className={styles.ButtonClick}
+              style={{ padding: 22, width: 'fit-content' }}
+            >
+              <IconPhone width={30} height={28} fill="#000" />
+            </Button>
+            <Typography.P weight="bold">LLAMAR</Typography.P>
+          </div>
+        </Flex>
+      </div>
       <NavigationOptions />
     </div>
   )
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,3 +1,5 @@
+import Footer from '@components/containers/Footer'
+
 import styles from './Home.module.css'
 
 const Home: React.FC = () => {
@@ -5,6 +7,7 @@ const Home: React.FC = () => {
     <section className={styles.container}>
       <h2>Hello from Home.tsx</h2>
       <p>Lets code!</p>
+      <Footer />
     </section>
   )
 }
