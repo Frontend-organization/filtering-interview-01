10bee0e2cb5c1a509e960518e7d125830df6b14d
ManuelDev <33798495+fullsnack-coder@users.noreply.github.com>
2021-09-01 22:11:53 -0500

    feat: add home (#26)
    * feat: add home
    
    * fix: centered BeersContainer
    
    * fix: NavigationOptions centered
    
    * test: add Home test
    
    Co-authored-by: Christian Guado <crguado@gmail.com>

--- src/components/containers/BeersContainer/BeersContainer.tsx
+++ src/components/containers/BeersContainer/BeersContainer.tsx
@@ -1,4 +1,4 @@
-import Card from '../Card'
+import Card from '@components/containers/Card'
 import ArrowRight from '@assets/icons/arrow-right.svg'
 import { Beer } from '@services/beerService'
 import Button from '@components/common/Button'
